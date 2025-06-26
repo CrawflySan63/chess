d4776077e9d393298a3f0805f35dc80a1bf64086
Steffan Newton <san63@byu.edu>
2025-06-26 17:06:40 -0600

    Logic added for Pawn to be able to capture, and promote and capture.

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -154,6 +154,27 @@ public class ChessPiece {
                 }
             }
 
+            for (int colOffset : new int[] {-1, 1}) {
+                int targetCol = startCol + colOffset;
+                int targetRow = startRow + direction;
+
+                if (targetCol >= 1 && targetCol <= 8 && targetRow >= 1 && targetRow <= 8) {
+                    ChessPosition diagPos = new ChessPosition(targetRow, targetCol);
+                    ChessPiece target = board.getPiece(diagPos);
+
+                    if (target != null && target.getTeamColor() != this.getTeamColor()) {
+                        // Promotion with capture
+                        if (targetRow == promotionRank) {
+                            for (PieceType promoteTo : new PieceType[] {PieceType.QUEEN, PieceType.ROOK, PieceType.BISHOP, PieceType.KNIGHT}) {
+                                moves.add(new ChessMove(myPosition, diagPos, promoteTo));
+                            }
+                        } else {
+                            moves.add(new ChessMove(myPosition, diagPos, null));
+                        }
+                    }
+                }
+            }
+
         } else if (type == PieceType.ROOK) {
             int[][] directions = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
 
