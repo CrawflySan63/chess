bfc67f122359ff029f3c20813f91fae358d2ce09
Steffan Newton <san63@byu.edu>
2025-07-15 12:11:38 -0600

    Added the record classes.

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,6 +1,7 @@
 package server;
 
 import spark.*;
+import server.handler.ClearHandler;
 
 public class Server {
 
@@ -10,9 +11,10 @@ public class Server {
         Spark.staticFiles.location("web");
 
         // Register your endpoints and handle exceptions here.
+        Spark.delete("/db", new ClearHandler());
 
         //This line initializes the server and can be removed once you have a functioning endpoint 
-        Spark.init();
+        //Spark.init();
 
         Spark.awaitInitialization();
         return Spark.port();
--- server/src/main/java/server/handler/ClearHandler.java
+++ server/src/main/java/server/handler/ClearHandler.java
@@ -1,4 +1,30 @@
 package server.handler;
 
-public class ClearHandler {
+import com.google.gson.Gson;
+import dataaccess.DataAccessException;
+import dataaccess.MemoryDataAccess;
+import service.ClearService;
+import spark.Request;
+import spark.Response;
+import spark.Route;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ClearHandler implements Route {
+    @Override
+    public Object handle(Request req, Response res) {
+        try {
+            ClearService service = new ClearService(new MemoryDataAccess());
+            service.clear();
+
+            res.status(200);
+            return "{}";
+        } catch (DataAccessException e) {
+            res.status(500);
+            Map<String, String> error = new HashMap<>();
+            error.put("message", "Error: " + e.getMessage());
+            return new Gson().toJson(error);
+        }
+    }
 }
