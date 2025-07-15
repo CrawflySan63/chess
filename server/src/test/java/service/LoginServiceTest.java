dc248c1763ba7e412d347573ebe40477829b6b59
Steffan Newton <san63@byu.edu>
2025-07-15 15:28:18 -0600

    Implemented Unit Test for Login Endpoint

---
+++ server/src/test/java/service/LoginServiceTest.java
@@ -0,0 +1,46 @@
+package service;
+
+import model.UserData;
+import dataaccess.DataAccess;
+import dataaccess.DataAccessException;
+import dataaccess.MemoryDataAccess;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import request.LoginRequest;
+import result.LoginResult;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class LoginServiceTest {
+    private UserService userService;
+
+    @BeforeEach
+    void setUp() throws DataAccessException {
+        DataAccess dataAccess = new MemoryDataAccess();
+        userService = new UserService(dataAccess);
+
+        //register a user for login testing
+        UserData user = new UserData("testuser", "password123", "email@example.com");
+        dataAccess.insertUser(user);
+    }
+
+    @Test
+    void login_success() throws DataAccessException {
+        LoginRequest request = new LoginRequest("testuser", "password123");
+        LoginResult result = userService.login(request);
+
+        assertEquals("testuser", result.username());
+        assertNotNull(result.authToken());
+    }
+
+    @Test
+    void login_wrongPassword_fails() {
+        LoginRequest badRequest = new LoginRequest("testuser", "wrongpass");
+
+        DataAccessException exception = assertThrows(DataAccessException.class, () -> {
+            userService.login(badRequest);
+        });
+
+        assertEquals("Error: unauthorized", exception.getMessage());
+    }
+}
