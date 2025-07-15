b1aff6538c6088d7c87dac0f8c96dc9fdf95cabd
Steffan Newton <san63@byu.edu>
2025-07-15 16:50:38 -0600

    Coupled LogoutHandler into Server.java

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -3,7 +3,9 @@ package server;
 import dataaccess.DataAccess;
 import dataaccess.MemoryDataAccess;
 import server.handler.LoginHandler;
+import server.handler.LogoutHandler;
 import server.handler.RegisterHandler;
+import service.LogoutService;
 import spark.*;
 import server.handler.ClearHandler;
 import service.UserService;
@@ -17,11 +19,13 @@ public class Server {
         //memory-based data store created
         DataAccess dataAccess = new MemoryDataAccess();
         UserService userService = new UserService(dataAccess);
+        LogoutService logoutService = new LogoutService(dataAccess);
 
         // Register your endpoints and handle exceptions here.
         Spark.delete("/db", new ClearHandler(dataAccess));
         Spark.post("/user", new RegisterHandler(userService));
         Spark.post("/session", new LoginHandler(userService));
+        Spark.delete("/session", new LogoutHandler(logoutService));
 
         //This line initializes the server and can be removed once you have a functioning endpoint 
         //Spark.init();
