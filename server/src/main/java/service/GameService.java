c3e5bb8485cab4318305bf37d25c41369e596531
Steffan Newton <san63@byu.edu>
2025-07-16 15:50:00 -0600

    Request and Result records were made for CreateGame. Result is just a string with gameID. Request is a string of gameName. GameService uses the authToken from the header and the gameName from the request body to create a new gameData with the new gameId and the gameName from the request body.

--- server/src/main/java/service/GameService.java
+++ server/src/main/java/service/GameService.java
@@ -1,12 +1,16 @@
 package service;
 
+import chess.ChessGame;
 import dataaccess.DataAccess;
 import dataaccess.DataAccessException;
 import model.AuthData;
 import model.GameData;
+import request.CreateGameRequest;
+import result.CreateGameResult;
 import result.ListGamesResult;
 
 import java.util.Collection;
+import java.util.UUID;
 
 public class GameService {
     private final DataAccess dataAccess;
@@ -27,5 +31,29 @@ public class GameService {
 
         //return the result by passing in collection of GameData objects made above
         return new ListGamesResult(games);
+    }
+
+    public CreateGameResult createGame(CreateGameRequest request, String authToken) throws DataAccessException {
+        //validate token
+        AuthData auth = dataAccess.getAuth(authToken);
+        if (auth == null) {
+            throw new DataAccessException("Error: unauthorized");
+        }
+
+        //validate gameName from the request
+        if (request.gameName() == null || request.gameName().isBlank()) {
+            throw new DataAccessException("Error: bad request");
+        }
+
+        //if both authToken and gameName  are valid, then generate gameID
+        int gameID = Math.abs(UUID.randomUUID().hashCode());
+
+        //create and insert GameData
+        GameData gameData = new GameData(gameID, null,  null, request.gameName(), new ChessGame());
+        dataAccess.insertGame(gameData);
+
+        //return success
+        return new CreateGameResult(gameID);
+
     }
 }
