f548710e23e9e762b3de48569a4a561571f27cfd
Steffan Newton <san63@byu.edu>
2025-07-16 22:40:56 -0600

    Making changes to code for better code quality. renamed gson to gSon in HandlerUtils.java

--- server/src/main/java/server/util/HandlerUtils.java
+++ server/src/main/java/server/util/HandlerUtils.java
@@ -6,7 +6,7 @@ import dataaccess.DataAccessException;
 
 public class HandlerUtils {
 
-    private static final Gson gson = new Gson();
+    private static final Gson gSon = new Gson();
 
     public static String handleException(Exception e, Response res) {
         if (e instanceof DataAccessException dae) {
@@ -18,10 +18,10 @@ public class HandlerUtils {
             } else {
                 res.status(500);
             }
-            return gson.toJson(new ErrorMessage(msg));
+            return gSon.toJson(new ErrorMessage(msg));
         } else {
             res.status(500);
-            return gson.toJson(new ErrorMessage("Error: " + e.getMessage()));
+            return gSon.toJson(new ErrorMessage("Error: " + e.getMessage()));
         }
     }
 
