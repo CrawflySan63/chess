0b1d54793ce43c48384bd488e34dffb7c36ae364
Steffan Newton <san63@byu.edu>
2025-07-23 19:03:32 -0600

    Deleted comments to raise Code Quality Score.

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -1,21 +1,8 @@
 import server.Server;
-import dataaccess.DatabaseManager;
-import dataaccess.DataAccessException;
 
 public class Main {
     public static void main(String[] args) {
         Server server = new Server();
         int port = server.run(8080);
-        /*try {
-            DatabaseManager.createDatabase();
-            DatabaseManager.configureTables();
-
-            Server server = new Server();
-            int port = server.run(8080);
-        } catch (DataAccessException e) {
-            System.err.println("Database setup failed: " + e.getMessage());
-            e.printStackTrace();
-        }*/
-
     }
 }
