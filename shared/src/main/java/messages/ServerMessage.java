854ba2dc9346b9b20ef6e8712b18d3f0907a4c42
Steffan Newton <san63@byu.edu>
2025-08-07 16:19:51 -0600

    Added starter files.

---
+++ shared/src/main/java/messages/ServerMessage.java
@@ -0,0 +1,67 @@
+package messages;
+
+import chess.ChessGame;
+
+import java.util.Objects;
+
+/**
+ * Represents a Message the server can send through a WebSocket
+ * 
+ * Note: You can add to this class, but you should not alter the existing
+ * methods.
+ */
+public class ServerMessage {
+    private ServerMessageType serverMessageType;
+
+    private ChessGame game;
+    private String message;
+
+    public enum ServerMessageType {
+        LOAD_GAME,
+        ERROR,
+        NOTIFICATION
+    }
+
+    public ServerMessage(ServerMessageType type) {
+        this.serverMessageType = type;
+    }
+
+    public ServerMessage(ServerMessageType type, ChessGame game) {
+        this.serverMessageType = type;
+        this.game = game;
+    }
+
+    public ServerMessage(ServerMessageType type, String message) {
+        this.serverMessageType = type;
+        this.message = message;
+    }
+
+    public ServerMessageType getServerMessageType() {
+        return this.serverMessageType;
+    }
+
+    public ChessGame getGame() {
+        return this.game;
+    }
+
+    public String getMessage() {
+        return this.message;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof ServerMessage)) {
+            return false;
+        }
+        ServerMessage that = (ServerMessage) o;
+        return getServerMessageType() == that.getServerMessageType();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getServerMessageType());
+    }
+}
